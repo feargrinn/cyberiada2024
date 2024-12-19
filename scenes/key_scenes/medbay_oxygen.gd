c8a8b93060b137c7583beb95a842d50561c3bd4e
Zapzap <zuzannaa.pietrzak@gmail.com>
2024-12-19 23:23:54 +0100

    qol globale
    jvhsdfjkbkjhlrlkksshvahg

---
+++ scenes/key_scenes/medbay_oxygen.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+func interact(player: Node2D) -> void:
+	print('powiększa się tlen')
--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -2,3 +2,13 @@ extends Node
 
 const TILE_SIZE = 8
 var urgency = 1.0 #goes down
+
+#dużo statystyk varowych
+var cisnienie = 100
+var energia = 100
+var kokpit = 100
+var lab = 100
+var o2 = 100
+var reaktor = 100
+var silnik = 100
+var workshop = 100
