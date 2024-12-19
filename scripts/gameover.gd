65ac64274e32d87ae2a1fb153556a6b9bbc7da6f
Zapzap <zuzannaa.pietrzak@gmail.com>
2024-12-19 20:00:57 +0100

    exitmenu
    image goes on top

---
+++ scripts/gameover.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for object in $MarginContainer/VBoxContainer/HBoxContainer.get_children():
+		if object is Button:
+			print(object.name)
+			object.connect("pressed", _on_button_pressed.bind(object.name))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	
+	pass
+
+func _on_button_pressed(button_name : String):
+	print('welp')
+	match button_name:
+		"quittomain":
+			get_tree().change_scene_to_file("res://scenes/key_scenes/main_menu.tscn")
+		"tryagain":
+			get_tree().change_scene_to_file("res://scenes/key_scenes/game.tscn")
+		"quit":
+			get_tree().quit()
