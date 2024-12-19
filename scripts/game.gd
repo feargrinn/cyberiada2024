832f59f2392ca96dd36ef4fb891e2b585e5328e0
Zapzap <zuzannaa.pietrzak@gmail.com>
2024-12-19 17:24:46 +0100

    MUSIC AFTER ALL THESE YEARS
    I finally managed to commit music
    also printtroll is now hashtaged

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _ready() -> void:
+	#print('trollprint')
+	$"AudioStreamPlayer".play()
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_released("ui_cancel"):
 		get_tree().change_scene_to_file("res://main_menu.tscn")
