d524ebc560e86a6162218e2017340de379f65a22
Regnihov <gapakiis@gmail.com>
2024-12-19 18:03:32 +0100

    Handling valve and wrench

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _ready() -> void:
--- scripts/wrench.gd
+++ scripts/wrench.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 func interact(player: Node2D) -> void:
 	var sprite = get_node_or_null("sprite")
-	if sprite:
+	if sprite and not player.get_node_or_null("sprite"):
 		remove_child(sprite)
 		player.add_child(sprite)
-	elif player.get_node_or_null("sprite"):
+	elif player.get_node_or_null("sprite") and not get_node_or_null("sprite"):
 		sprite = player.get_node("sprite")
 		if(sprite != null):
 			player.remove_child(sprite)
