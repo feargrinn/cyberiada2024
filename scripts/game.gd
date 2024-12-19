0a8544f7fe1961eba7bd2679ad31ea6555d846d4
Regnihov <gapakiis@gmail.com>
2024-12-19 20:00:26 +0100

    Added files

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,7 +3,6 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _ready() -> void:
-	#print('trollprint')
 	$"AudioStreamPlayer".play()
 
 func _process(_delta: float) -> void:
