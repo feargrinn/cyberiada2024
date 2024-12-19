ca28d87b6812eb19dfdbf1f02c7e36719d47a823
Regnihov <gapakiis@gmail.com>
2024-12-19 21:40:41 +0100

    Removing some bugs

--- scripts/game.gd
+++ scripts/game.gd
@@ -11,15 +11,14 @@ func _process(_delta: float) -> void:
 		get_tree().change_scene_to_file("res://scenes/key_scenes/main_menu.tscn")
 
 func lose(why = "No reasyon at all"):
-	$AudioStreamPlayer2
 	var gameovar = scene.instantiate()
 	
 	print("You lost ", why, " :(")
 	add_child(gameovar) 
 	gameovar.losingtext.text = why
 	remove_child(gameovar)
-	var scene = PackedScene.new()
-	scene.pack(gameovar)
-	get_tree().change_scene_to_packed(scene)
+	var packed_scene = PackedScene.new()
+	packed_scene.pack(gameovar)
+	get_tree().change_scene_to_packed(packed_scene)
 	
 	
